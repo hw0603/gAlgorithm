0d60be9c78e9366aff00c33ecc33def653b17e35
임수빈 <robin980108@naver.com>
2024-07-08 18:41:10 +0900

    25409 추가

---
+++ week10/25049/robinjoon/Main.java
@@ -0,0 +1,45 @@
+package org.example.p25049;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] arr = new long[n+1];
+        String line = br.readLine();
+        StringTokenizer st = new StringTokenizer(line);
+        for (int i = 1; i <= n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long[] dp = new long[n+1];
+        long[] dpdp = new long[n+1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = Math.max(dp[i-1] + arr[i], arr[i]);
+            dpdp[i-1] = Math.max(dpdp[i],dp[i-1]);
+        }
+        dpdp[n] = Math.max(dpdp[n],dp[n]);
+        long max = Long.MIN_VALUE;
+        long[] dp2 = new long[n+1];
+        long[] dp3 = new long[n+1];
+        for (int i = n-1; i >=0; i--) {
+            dp2[i] = Math.max(dp2[i+1]+arr[i+1],arr[i+1]);
+            dp3[i] = Math.max(dp3[i+1],dp2[i]);
+        }
+        for (int i = 1; i <= n; i++) {
+            long lMax = dpdp[i];// 왼쪽 구간
+            long rMax = dp3[i];
+            max = Math.max(rMax + lMax, max);
+        }
+        max = Math.max(0,max);
+        long sum = Arrays.stream(arr).sum();
+        max = Math.max(max,sum);
+        System.out.println(sum + max);
+    }
+}
+
